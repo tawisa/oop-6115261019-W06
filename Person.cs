66629f46e1fff948d9fde9b3bb6cc1db9b43c962
tawisa <68209105+tawisa@users.noreply.github.com>
2020-08-20 09:47:10 +0700

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_6115261019_w06
+{
+    class Person
+    {
+        private string name;
+        private string lastname;
+        private double salary;
+        private string position;
+
+        public string Name
+        {
+            get { return name; }
+            set { this.name = value; }
+        }
+        public string Lastname
+        {
+            get { return lastname; }
+            set { this.lastname = value; }
+        }
+        public double Salary
+        {
+            get { return salary; }
+            set { this.salary = value; }
+        }
+        public string Position
+        {
+            get { return position; }
+            set { this.position = value; }
+        }
+
+        public Person() { }
+        public Person(string Name, string Lastname, double Salary, string Position)
+        {
+            this.Name = Name;
+            this.Lastname = Lastname;
+            this.Salary = Salary;
+            this.Position = Position;
+
+        }
+        public override string ToString()
+        {
+            return this.Name + " " + this.Lastname + " salary : " + this.Salary + " Tax : " + Tax.getTax(this.Salary * 12);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace oop_6115261019_w06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Tawisa", "", 20000, "Lecturer");
+            Person p2 = new Person("Suwannee", "", 25000, "Lecturer");
+            Person p3 = new Person("Supansa", "", 30000, "Staff");
+            Faculty f1 = new Faculty("Faculty of Educational");
+            f1.setPerson(p1, 0);
+            f1.setPerson(p2, 1);
+            f1.setPerson(p3, 2);
+            f1.printAll();
+
+        }
+    }
+}
